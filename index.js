c1f4d288071917821f5c51463073db2cb42d171d
Khadija ismail <khadijaismail696@gmail.com>
2025-05-08 21:00:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-
+function changeColour(){
+    let body = document.querySelector("body")
+    if(body.classList.contains("dark_theme")){
+        body.classList.remove("dark_theme")
+    }else{
+        body.classList.add("dark_theme")
+    }
+}
+document.querySelector(".dark").addEventListener("click", changeColour)
+function joinBookClub(){
+    let name = prompt("Enter your Full Name:")
+    alert("Hello " + name + ", Welcome to Khadija's Book Club")
+}
+document.querySelector("#book").addEventListener("click", joinBookClub)  
+   
